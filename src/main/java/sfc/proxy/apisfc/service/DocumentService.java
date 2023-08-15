7a27143e60fa9bbe235d48a3c99684fa64c8d55e
harqmusic <hreyesq@yahoo.com>
2023-08-14 21:36:26 -0500

    da

--- src/main/java/sfc/proxy/apisfc/service/DocumentService.java
+++ src/main/java/sfc/proxy/apisfc/service/DocumentService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class DocumentService {
 
     public String getGoogleDocument(String googleUrl) {
-        return googleUrl + "SIII";
+        return "DATOS INGRESADOS" + googleUrl;
     }
 
 
